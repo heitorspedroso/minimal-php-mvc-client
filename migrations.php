74cfc92467b62fcefdc038fc88bec98d6ca8116b
Heitor <heitorspedroso@gmail.com>
2021-04-20 09:58:45 -0300

    version 1.0 before transform framework in package

---
+++ migrations.php
@@ -0,0 +1,21 @@
+<?php
+
+use app\core\Application;
+
+//now every class if we use it with the correct namespace can be autoloaded
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$config= [
+	'db' => [
+		'dsn'=>$_ENV['DB_DSN'],
+		'user'=>$_ENV['DB_USER'],
+		'password'=>$_ENV['DB_PASSWORD'],
+	]
+];
+
+$app = new Application(__DIR__, $config);
+
+$app->db->applyMigrations();
